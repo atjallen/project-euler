192fac6a781d39ea141c4b7ad4d10e6cd0c58e66
Ashley Allen <ashleytjallen@gmail.com>
2022-03-11 14:51:16 +0000

    Add additional solution to 48

--- 048/solution.js
+++ 048/solution.js
@@ -1,11 +1,24 @@
 // Trivial brute-force solution using BigInt
-
 function solve(n) {
     let sum = BigInt(0);
     for (let i = BigInt(1); i <= n; i++) {
         sum += i ** i;
     }
     return sum.toString().slice(-10)
+}
+
+// Slightly cleverer solution using masks to continually
+// mask the last 10 digits and avoid overflows
+function solve2(n) {
+    let sum = 0;
+    for (let i = 1; i <= n; i++) {
+        let product = 1;
+        for (let j = 1; j <= i; j++) {
+            product = (product * i) & 1_111_111_111;
+        }
+        sum = (sum + product) & 1_111_111_111;
+    }
+    return sum;
 }
 
 const input = parseInt(process.argv[2]) || 1000;
