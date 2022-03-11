c388013a1496c9a36fdc63f0c45722f56c523b16
Ashley Allen <ashleytjallen@gmail.com>
2022-03-11 16:36:54 +0000

    Fix solution

--- 048/solution.js
+++ 048/solution.js
@@ -7,6 +7,10 @@ function solve(n) {
     return sum.toString().slice(-10)
 }
 
+function trimToNumDigits(n, nDigits) {
+    return parseInt(n.toString().slice(-nDigits));
+}
+
 // Slightly cleverer solution using masks to continually
 // mask the last 10 digits and avoid overflows
 function solve2(n) {
@@ -14,12 +18,13 @@ function solve2(n) {
     for (let i = 1; i <= n; i++) {
         let product = 1;
         for (let j = 1; j <= i; j++) {
-            product = (product * i) & 1_111_111_111;
+            product = trimToNumDigits(product * i, 10);
         }
-        sum = (sum + product) & 1_111_111_111;
+        sum = trimToNumDigits(sum + product, 10);
     }
     return sum;
 }
 
 const input = parseInt(process.argv[2]) || 1000;
 console.log(solve(input));
+console.log(solve2(input));
